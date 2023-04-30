fb67c09a750a4baa76727ae848f8317575785ba5
gustavokubiack <guukubiack@gmail.com>
2023-04-30 12:09:33 -0300

    Include decimal_to_binary function.

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.decimal import Decimal
+
+num = int(input("Enter a number: "))
+base = int(input("Enter a base: "))
+convert_to = int(input("Enter a base to convert to: "))
+
+def main():
+    decimal = Decimal(num, base, convert_to)
+    decimal.decimal_to_binary()
+
+if __name__ == "__main__":
+    main()
---
+++ src/decimal.py
@@ -0,0 +1,11 @@
+class Decimal:
+
+    def __init__(self, num, base, convert_to):
+        self.num = num
+        self.base = base 
+        self.convert_to = convert_to
+
+    def decimal_to_binary(self):
+        if self.base == 10 and self.convert_to == 2:
+            print (bin(self.num)[2:])
+
